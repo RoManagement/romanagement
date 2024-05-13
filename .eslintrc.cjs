e1cf53dab3ba54ad1acc89378f6b1047382b1f24
Evan <123332223+EvanDevvv@users.noreply.github.com>
2024-05-12 20:05:51 -0400

    chore: Disable "@typescript-eslint/restrict-template-expressions" rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,7 @@ const config = {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
 
     "@typescript-eslint/consistent-type-imports": [
       "warn",
