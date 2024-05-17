d45bb9a1b120b558f65ffda455a8dbfd59903337
Evan <123332223+EvanDevvv@users.noreply.github.com>
2024-05-16 22:41:40 -0400

    chore: Disable unnecessary type assertion rule in ESLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,7 @@ const config = {
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unnecessary-type-assertion": "off",
 
     "@typescript-eslint/consistent-type-imports": [
       "warn",
